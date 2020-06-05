6da40aab531bede7968cf7216283a37b8068c5ea
Andrey Druzhkin <andrey.druzhkin@gmail.com>
2020-06-04 21:04:06 -0600

    Added printBoard, setAlive and setDead

--- src/main/java/com/adruzhkin/Simulation.java
+++ src/main/java/com/adruzhkin/Simulation.java
@@ -11,4 +11,40 @@ public class Simulation {
         this.height = height;
         this.board = new int[width][height];
     }
+
+    public void printBoard() {
+        System.out.println("---");
+        for (int y = 0; y < height; y++) {
+            StringBuilder line = new StringBuilder("|");
+            for (int x = 0; x < width; x++) {
+                if (this.board[x][y] == 0) {
+                    line.append(".");
+                } else {
+                    line.append("*");
+                }
+            }
+            line.append("|");
+            System.out.println(line);
+        }
+        System.out.println("---\n");
+    }
+
+    public void setAlive(int x, int y) {
+        this.board[x][y] = 1;
+    }
+
+    public void setDead(int x, int y) {
+        this.board[x][y] = 0;
+    }
+
+    public static void main(String[] args) {
+        Simulation simulation = new Simulation(8, 5);
+
+        simulation.setAlive(2, 2);
+        simulation.setAlive(3, 2);
+        simulation.setAlive(4, 2);
+
+        simulation.printBoard();
+    }
+
 }
