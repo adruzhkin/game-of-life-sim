89910762a55401d596af0868dca0e300bd0b9650
Andrey Druzhkin <andrey.druzhkin@gmail.com>
2020-06-04 21:25:58 -0600

    Added alive cells counter method

--- src/main/java/com/adruzhkin/Simulation.java
+++ src/main/java/com/adruzhkin/Simulation.java
@@ -37,6 +37,28 @@ public class Simulation {
         this.board[x][y] = 0;
     }
 
+    public int countAliveNeighbours(int x, int y) {
+        int count = 0;
+
+        int xLess = Math.max(x - 1, 0);
+        int xMore = Math.min(x + 1, width - 1);
+        int yLess = Math.max(y - 1, 0);
+        int yMore = Math.min(y + 1, height - 1);
+
+        count += this.board[xLess][yLess];
+        count += this.board[x][yLess];
+        count += this.board[xMore][yLess];
+
+        count += this.board[xLess][y];
+        count += this.board[xMore][y];
+
+        count += this.board[xLess][yMore];
+        count += this.board[x][yMore];
+        count += this.board[xMore][yMore];
+
+        return count;
+    }
+
     public static void main(String[] args) {
         Simulation simulation = new Simulation(8, 5);
 
@@ -45,6 +67,8 @@ public class Simulation {
         simulation.setAlive(4, 2);
 
         simulation.printBoard();
+
+        System.out.println(simulation.countAliveNeighbours(7, 4));
     }
 
 }
