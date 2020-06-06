749edc02733ca1bb6442797f41c7937bd310351d
Andrey Druzhkin <andrey.druzhkin@gmail.com>
2020-06-05 18:05:27 -0600

    Removed test method from Simulation

--- src/main/java/com/adruzhkin/Simulation.java
+++ src/main/java/com/adruzhkin/Simulation.java
@@ -102,23 +102,4 @@ public class Simulation {
         board = newBoard;
     }
 
-    public static void main(String[] args) {
-        Simulation simulation = new Simulation(8, 5);
-
-        simulation.setAlive(2, 2);
-        simulation.setAlive(3, 2);
-        simulation.setAlive(4, 2);
-
-        simulation.printBoard();
-
-        simulation.step();
-        simulation.printBoard();
-        simulation.step();
-        simulation.printBoard();
-        simulation.step();
-        simulation.printBoard();
-        simulation.step();
-        simulation.printBoard();
-    }
-
 }
