28eab6b07eac027522767f2fdd9659d98a54081f
Andrey Druzhkin <andrey.druzhkin@gmail.com>
2020-06-07 15:15:09 -0600

    Refactored Simulation class to use Board and SimulationRule interfaces

--- src/main/java/com/adruzhkin/gol/Simulation.java
+++ src/main/java/com/adruzhkin/gol/Simulation.java
@@ -1,120 +1,34 @@
 package com.adruzhkin.gol;
 
+import com.adruzhkin.gol.model.Board;
+import com.adruzhkin.gol.model.CellState;
+import com.adruzhkin.gol.model.SimulationRule;
+
 public class Simulation {
 
-    public static final int DEAD = 0;
-    public static final int ALIVE = 1;
+    private Board simulationBoard;
+    private SimulationRule simulationRule;
 
-    private int width;
-    private int height;
-    private int[][] board;
-
-    public Simulation(int width, int height) {
-        this.width = width;
-        this.height = height;
-        this.board = new int[width][height];
+    public Simulation(Board simulationBoard, SimulationRule simulationRule) {
+        this.simulationBoard = simulationBoard;
+        this.simulationRule = simulationRule;
     }
 
-    public static Simulation copy(Simulation simulation) {
-        Simulation copy = new Simulation(simulation.width, simulation.height);
-
-        for (int y = 0; y < simulation.height; y++) {
-            for (int x = 0; x < simulation.width; x++) {
-                copy.setState(x, y, simulation.getState(x, y));
-            }
-        }
-
-        return copy;
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public void printBoard() {
-        System.out.println("---");
-        for (int y = 0; y < height; y++) {
-            StringBuilder line = new StringBuilder("|");
-            for (int x = 0; x < width; x++) {
-                if (this.board[x][y] == DEAD) {
-                    line.append(".");
-                } else {
-                    line.append("*");
-                }
-            }
-            line.append("|");
-            System.out.println(line);
-        }
-        System.out.println("---\n");
-    }
-
-    public void setAlive(int x, int y) {
-        this.setState(x, y, ALIVE);
-    }
-
-    public void setDead(int x, int y) {
-        this.setState(x, y, DEAD);
-    }
-
-    public boolean isAlive(int x, int y) {
-        return this.board[x][y] == ALIVE;
-    }
-
-    public boolean isDead(int x, int y) {
-        return this.board[x][y] == DEAD;
-    }
-
-    public int getState(int x, int y) {
-        if (x < 0 || x >= width) return DEAD;
-        if (y < 0 || y >= height) return DEAD;
-        return board[x][y];
-    }
-
-    public void setState(int x, int y, int state) {
-        if (x < 0 || x >= width) return;
-        if (y < 0 || y >= height) return;
-        board[x][y] = state;
-    }
-
-    public int countAliveNeighbours(int x, int y) {
-        int count = 0;
-
-        count += getState(x - 1, y - 1);
-        count += getState(x, y - 1);
-        count += getState(x + 1, y - 1);
-
-        count += getState(x - 1, y);
-        count += getState(x + 1, y);
-
-        count += getState(x - 1, y + 1);
-        count += getState(x, y + 1);
-        count += getState(x + 1, y + 1);
-
-        return count;
+    public Board getBoard() {
+        return simulationBoard;
     }
 
     public void step() {
-        int[][] newBoard = new int[width][height];
+        Board nextBoard = this.simulationBoard.copy();
 
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                int aliveNeighbours = countAliveNeighbours(x, y);
-                newBoard[x][y] = board[x][y];
-
-                if (isAlive(x, y)) {
-                    if (aliveNeighbours < 2 || aliveNeighbours > 3) newBoard[x][y] = DEAD;
-                } else {
-                    if (aliveNeighbours == 3) newBoard[x][y] = ALIVE;
-                }
-
+        for (int y = 0; y < simulationBoard.getHeight(); y++) {
+            for (int x = 0; x < simulationBoard.getWidth(); x++) {
+                CellState nextState = simulationRule.getNextState(x, y, simulationBoard);
+                nextBoard.setState(x, y, nextState);
             }
         }
 
-        board = newBoard;
+        this.simulationBoard = nextBoard;
     }
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,5 @@
 module com.adruzhkin {
     requires javafx.controls;
     exports com.adruzhkin.gol;
+    exports com.adruzhkin.gol.model;
 }
