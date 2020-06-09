2ed2fdcf0c64d2412b39cc580c6bd8b3e66b7524
Andrey Druzhkin <andrey.druzhkin@gmail.com>
2020-06-09 13:19:39 -0600

    Added EditorViewModel

---
+++ src/main/java/com/adruzhkin/gol/viewmodel/EditorViewModel.java
@@ -0,0 +1,52 @@
+package com.adruzhkin.gol.viewmodel;
+
+import com.adruzhkin.gol.model.Board;
+import com.adruzhkin.gol.model.CellState;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class EditorViewModel {
+
+    private CellState drawMode = CellState.ALIVE; //Default mode
+    private List<SimpleChangeListener<CellState>> drawModeListeners;
+
+    private BoardViewModel boardViewModel;
+    private Board editorBoard;
+    private boolean drawingEnabled;
+
+    public EditorViewModel(BoardViewModel boardViewModel, Board initialBoard) {
+        this.boardViewModel = boardViewModel;
+        this.editorBoard = initialBoard;
+        this.drawModeListeners = new LinkedList<>();
+    }
+
+    public CellState getDrawMode() {
+        return this.drawMode;
+    }
+
+    public void setDrawMode(CellState drawMode) {
+        this.drawMode = drawMode;
+        this.notifyDrawModeListeners();
+    }
+
+    private void notifyDrawModeListeners() {
+        for (SimpleChangeListener<CellState> drawModeListener : drawModeListeners) {
+            drawModeListener.valueChanged(this.drawMode);
+        }
+    }
+
+    public void listenToDrawMode(SimpleChangeListener<CellState> listener) {
+        this.drawModeListeners.add(listener);
+    }
+
+    public void onAppStateChanged(ApplicationState state) {
+        if (state == ApplicationState.EDITING) {
+            this.drawingEnabled = true;
+            this.boardViewModel.setBoard(editorBoard);
+        } else {
+            this.drawingEnabled = false;
+        }
+    }
+
+}
