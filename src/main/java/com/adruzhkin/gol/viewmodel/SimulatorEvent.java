def8c35334f69ccb62fed1bd0c0de8859e4c164e
Andrey Druzhkin <andrey.druzhkin@gmail.com>
2020-06-15 16:30:41 -0600

    Added SimulatorEvent class to track pressed buttons from SimulationViewModel

---
+++ src/main/java/com/adruzhkin/gol/viewmodel/SimulatorEvent.java
@@ -0,0 +1,21 @@
+package com.adruzhkin.gol.viewmodel;
+
+public class SimulatorEvent {
+
+    public enum Type {
+        START,
+        STOP,
+        STEP,
+        RESET
+    }
+
+    private Type eventType;
+
+    public SimulatorEvent(Type eventType) {
+        this.eventType = eventType;
+    }
+
+    public Type getEventType() {
+        return eventType;
+    }
+}
